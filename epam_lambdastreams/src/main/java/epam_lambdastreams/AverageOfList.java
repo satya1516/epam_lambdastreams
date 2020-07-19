54d2d330d430cd8989aa4b7e72dbbbab9c5be778
DELL <DELL@192.168.0.3>
2020-07-19 15:08:32 +0530

    task completed

---
+++ epam_lambdastreams/src/main/java/epam_lambdastreams/AverageOfList.java
@@ -0,0 +1,25 @@
+package epam_lambdastreams;
+
+import java.util.*;
+
+public class AverageOfList {
+	public static void main(String[] args) {
+		List<Integer> num = Arrays.asList(1, 2, 3, 4, 5);
+		double avg = AverageOfList.doaverage(num);
+		System.out.println(avg);
+	  }
+
+	private static double doaverage(List <Integer> marks) {
+	    if (marks == null || marks.isEmpty()) {
+	        return 0;
+	    }
+
+	    double sum = 0;
+	    for (Integer mark : marks) {
+	        sum += mark;
+	    }
+
+	    return sum / marks.size();
+	}
+
+}
